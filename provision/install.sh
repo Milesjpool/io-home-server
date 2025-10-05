62e743a7d246b09d85bbaa86c2063ee0bf72d660
Miles Pool <milesjpool@gmail.com>
2025-10-05 03:03:20 +0100

    Add custom SSH port

--- provision/install.sh
+++ provision/install.sh
@@ -7,6 +7,16 @@ for pkg in $(cat pkglist); do
   sudo apt-get install -y $pkg;
 done
 
+
+if ! sudo ss -tlnp | grep -q sshd; then
+  read -p "SSH Port [22]: " input_port
+  SSH_PORT=${input_port:-22}
+
+  echo "PasswordAuthentication no" | sudo tee -a /etc/ssh/sshd_config
+  echo "Port $SSH_PORT" | sudo tee -a /etc/ssh/sshd_config
+
+  sudo ufw allow $SSH_PORT/tcp
+fi
+
 sudo systemctl enable --now ssh
-sudo ufw allow ssh
-sudo ufw enable
+echo 'y' | sudo ufw enable
