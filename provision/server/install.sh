1a6d90196c3e520ebed09923dba4a8858ae120af
Miles Pool <milesjpool@gmail.com>
2025-10-05 03:03:47 +0100

    Add headless docker

--- provision/server/install.sh
+++ provision/server/install.sh
@@ -2,8 +2,27 @@
 
 (cd ..; ./install.sh)
 
+
+# Add Docker's official GPG key:
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+
 for pkg in $(cat pkglist); do
   sudo apt-get install -y $pkg;
 done
 
 gsettings set org.gnome.SessionManager logout-prompt false
+
+sudo systemctl enable --now docker
+USER_TO_ADD=${SUDO_USER:-$USER}
+sudo usermod -aG docker "$USER_TO_ADD"
+
