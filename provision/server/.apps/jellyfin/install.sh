432242f6fd141d954aacecf4003e167312bcca0c
Miles Pool <milesjpool@gmail.com>
2025-10-08 00:21:09 +0100

    Add jellyfin app config

---
+++ provision/server/.apps/jellyfin/install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+SVC_USER='svc-jellyfin'
+SVC_HOME='/srv/jellyfin'
+
+sudo mkdir -p $SVC_HOME
+
+sudo useradd -r -s /usr/sbin/nologin -d $SVC_HOME $SVC_USER
+sudo chown -R $SVC_USER:$SVC_USER $SVC_HOME
+
+sudo usermod -aG users $SVC_USER
+
+USER_UID="$(id $SVC_USER -u)" \
+  USER_GID="$(id $SVC_USER -g)" \
+  docker compose up -d
