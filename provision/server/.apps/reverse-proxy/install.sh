c524699bc92c1295496690620f2954cfa630d309
Miles Pool <milesjpool@gmail.com>
2025-10-08 00:22:29 +0100

    Add initial revproxy config.

---
+++ provision/server/.apps/reverse-proxy/install.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+SVC_USER='svc-revproxy'
+SVC_HOME='/srv/revproxy'
+
+sudo mkdir -p $SVC_HOME
+
+sudo useradd -r -s /usr/sbin/nologin -d $SVC_HOME $SVC_USER
+sudo chown -R $SVC_USER:$SVC_USER $SVC_HOME
+
+USER_UID="$(id $SVC_USER -u)" \
+  USER_GID="$(id $SVC_USER -g)" \
+  docker compose up -d
+
