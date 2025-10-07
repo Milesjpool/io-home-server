65fdef0dde6de26f442e7178007945deeb98ffe2
Miles Pool <milesjpool@gmail.com>
2025-10-08 00:32:26 +0100

    Add homeassistant app config

---
+++ provision/server/.apps/home-assistant/install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+SVC_USER='svc-homeassistant'
+SVC_HOME='/srv/homeassistant'
+
+sudo mkdir -p $SVC_HOME
+
+sudo useradd -r -s /usr/sbin/nologin -d $SVC_HOME $SVC_USER
+sudo chown -R $SVC_USER:$SVC_USER $SVC_HOME
+
+sudo usermod -aG users $SVC_USER
+
+USER_UID="$(id $SVC_USER -u)" \
+  USER_GID="$(id $SVC_USER -g)" \
+  docker compose up -d
