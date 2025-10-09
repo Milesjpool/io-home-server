7ece8aaa3d7f061f6471b9e8ca2fa71ac846abf3
Miles Pool <milesjpool@gmail.com>
2025-10-10 00:57:14 +0100

    Bind adguard to LAN network

--- provision/server/.apps/adguard-home/install.sh
+++ provision/server/.apps/adguard-home/install.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+source ../global.env
+
 SVC_USER='svc-adguard'
 SVC_HOME='/srv/adguard'
 
@@ -8,6 +10,11 @@ sudo mkdir -p $SVC_HOME
 sudo useradd -r -s /usr/sbin/nologin -d $SVC_HOME $SVC_USER
 sudo chown -R $SVC_USER:$SVC_USER $SVC_HOME
 
-USER_UID="$(id $SVC_USER -u)" \
+SERVER_IP=$(hostname -I | awk '{print $1}')
+sudo ufw allow from $NETMASK to $SERVER_IP port 53 proto udp
+sudo ufw allow from $NETMASK to $SERVER_IP port 53 proto tcp
+
+DNS_BIND_IP="$SERVER_IP" \
+  USER_UID="$(id $SVC_USER -u)" \
   USER_GID="$(id $SVC_USER -g)" \
   docker compose up -d
