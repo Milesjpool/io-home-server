dc62ff1c8140306d0a66298b635ab6dc461185a5
Miles Pool <milesjpool@gmail.com>
2025-10-11 00:45:27 +0100

    Update Docker configurations for AdGuard Home and reverse proxy to allow LAN access on standard ports

--- provision/server/.apps/adguard-home/install.sh
+++ provision/server/.apps/adguard-home/install.sh
@@ -10,11 +10,9 @@ sudo mkdir -p $SVC_HOME
 sudo useradd -r -s /usr/sbin/nologin -d $SVC_HOME $SVC_USER
 sudo chown -R $SVC_USER:$SVC_USER $SVC_HOME
 
-SERVER_IP=$(hostname -I | awk '{print $1}')
-sudo ufw allow from $NETMASK to $SERVER_IP port 53 proto udp
-sudo ufw allow from $NETMASK to $SERVER_IP port 53 proto tcp
+sudo ufw allow from $NETMASK to any port 53 proto udp
+sudo ufw allow from $NETMASK to any port 53 proto tcp
 
-DNS_BIND_IP="$SERVER_IP" \
-  USER_UID="$(id $SVC_USER -u)" \
+USER_UID="$(id $SVC_USER -u)" \
   USER_GID="$(id $SVC_USER -g)" \
   docker compose up -d
--- provision/server/.apps/reverse-proxy/install.sh
+++ provision/server/.apps/reverse-proxy/install.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+source ../global.env
+
 SVC_USER='svc-revproxy'
 SVC_HOME='/srv/revproxy'
 
@@ -7,6 +9,9 @@ sudo mkdir -p $SVC_HOME
 
 sudo useradd -r -s /usr/sbin/nologin -d $SVC_HOME $SVC_USER
 sudo chown -R $SVC_USER:$SVC_USER $SVC_HOME
+
+sudo ufw allow from $NETMASK to any port 80 proto tcp
+sudo ufw allow from $NETMASK to any port 443 proto tcp
 
 USER_UID="$(id $SVC_USER -u)" \
   USER_GID="$(id $SVC_USER -g)" \
