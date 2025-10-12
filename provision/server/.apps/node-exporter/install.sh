f5ada702ae0f382e8dc288768e178c0b9d007970
Miles Pool <milesjpool@gmail.com>
2025-10-12 21:26:35 +0100

    cleanup node-exporter.

--- provision/server/.apps/node-exporter/install.sh
+++ provision/server/.apps/node-exporter/install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Load Docker network config
 source ../global.env
 
-# Allow 'proxy' bridge network to access node-exporter on host
 sudo ufw allow from $DOCKER_SUBNET to any port 9100 proto tcp
 
-# Node exporter runs as root to access host metrics
 docker compose up -d
 
