5a6c457987ebc8d5c1363c844be042c1391538ed
Miles Pool <milesjpool@gmail.com>
2025-10-12 14:52:33 +0100

    Move node-exporter to 'host' mode for better monitoring,

--- provision/server/.apps/node-exporter/install.sh
+++ provision/server/.apps/node-exporter/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Allow 'proxy' bridge network to access node-exporter on host
+sudo ufw allow from 172.20.0.0/16 to any port 9100 proto tcp
+
 # Node exporter runs as root to access host metrics
 docker compose up -d
 
