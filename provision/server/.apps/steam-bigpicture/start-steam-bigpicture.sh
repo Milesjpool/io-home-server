1036d3b6e7157bb95e1d2e7cd9f4e3f5768cef17
Miles Pool <milesjpool@gmail.com>
2025-10-22 23:11:49 +0100

    Configure steam runtime env.

--- provision/server/.apps/steam-bigpicture/start-steam-bigpicture.sh
+++ provision/server/.apps/steam-bigpicture/start-steam-bigpicture.sh
@@ -2,15 +2,21 @@
 
 USER_UID="$1"
 export DISPLAY=:0
-export XDG_RUNTIME_DIR=/run/user/$USER_UID
 
 # Start X server as root (for console access)
-/usr/bin/X :0 -nocursor -nolisten tcp -ac &
+/usr/bin/X :0 -nocursor -nolisten tcp -ac -dpi 96 -s 0 -dpms &
 X_PID=$!
 until xset q &>/dev/null; do sleep 0.1; done
 
 # Start Steam Big Picture as user with preserved environment
-sudo -u#$USER_UID -E bash -c "
+sudo -u#$USER_UID bash -c "
+  export MESA_VK_DEVICE_SELECT=0
+  export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/intel_icd.x86_64.json
+  
+  # Bluetooth controller support
+  export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
+  export XDG_RUNTIME_DIR=/run/user/$USER_UID
+  
   openbox &
   sleep 0.5
   steam -bigpicture -fulldesktopres
