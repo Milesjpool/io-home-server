fb7762361a4afe45b93a5a0e78fe501689c5e33f
Miles Pool <milesjpool@gmail.com>
2025-11-09 01:20:23 +0000

    Add fail2ban configuration files and installation script for enhanced security

---
+++ provision/server/.apps/fail2ban/install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+source ../../../private.env
+
+set -euo pipefail
+
+sudo apt-get update -y
+sudo apt-get install -y fail2ban
+
+sudo sed "s|__SSHD_PORT__|${SSHD_PORT:-22}|g" "jail.local.template" | sudo tee /etc/fail2ban/jail.local >/dev/null
+sudo cp "caddy-public.conf" /etc/fail2ban/filter.d/caddy-public.conf
+sudo cp "dc-iptables-multiport.conf" /etc/fail2ban/action.d/dc-iptables-multiport.conf
+
+sudo systemctl enable fail2ban
+sudo systemctl restart fail2ban
