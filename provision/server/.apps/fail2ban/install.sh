8bfa963d967b3e79d4911bf00098e5c30d015fdb
Miles Pool <milesjpool@gmail.com>
2025-11-22 04:08:54 +0000

    move f2b filters into subdirectory, and rename filter.

--- provision/server/.apps/fail2ban/install.sh
+++ provision/server/.apps/fail2ban/install.sh
@@ -8,7 +8,7 @@ sudo apt-get update -y
 sudo apt-get install -y fail2ban
 
 sudo sed "s|__SSHD_PORT__|${SSHD_PORT:-22}|g" "jail.local.template" | sudo tee /etc/fail2ban/jail.local >/dev/null
-sudo cp "caddy-public.conf" /etc/fail2ban/filter.d/caddy-public.conf
+sudo cp filter.d/* /etc/fail2ban/filter.d/
 sudo cp "dc-iptables-multiport.conf" /etc/fail2ban/action.d/dc-iptables-multiport.conf
 
 sudo systemctl enable fail2ban
