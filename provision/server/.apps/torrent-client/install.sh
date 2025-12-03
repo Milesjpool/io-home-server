d7abb6cdd8a7fa532881541546aee6318d963c29
Miles Pool <milesjpool@gmail.com>
2025-12-03 00:10:19 +0000

    Scrape qbt logs to loki.

--- provision/server/.apps/torrent-client/install.sh
+++ provision/server/.apps/torrent-client/install.sh
@@ -10,7 +10,7 @@ SVC_HOME='/srv/torrent-client'
 sudo useradd -r -s /usr/sbin/nologin -d $SVC_HOME $SVC_USER
 
 sudo mkdir -p $SVC_HOME/gluetun
-sudo mkdir -p $SVC_HOME/qbittorrent/config
+sudo mkdir -p $SVC_HOME/qbittorrent/config/logs
 sudo mkdir -p /mnt/torrents
 
 sudo chown -R $SVC_USER:$SVC_USER $SVC_HOME
