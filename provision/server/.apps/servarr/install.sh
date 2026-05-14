f320f2fb66dec4cc6d99cb3993f14a6db7df3374
Miles Pool <milesjpool@gmail.com>
2026-05-14 13:29:01 +0100

    Add radarr to servarr stack

--- provision/server/.apps/servarr/install.sh
+++ provision/server/.apps/servarr/install.sh
@@ -5,20 +5,16 @@ SVC_HOME='/srv/servarr'
 
 sudo mkdir -p $SVC_HOME/prowlarr
 sudo mkdir -p $SVC_HOME/lidarr
+sudo mkdir -p $SVC_HOME/radarr
 
 sudo useradd -r -s /usr/sbin/nologin -d $SVC_HOME $SVC_USER
 sudo chown -R $SVC_USER:$SVC_USER $SVC_HOME
 
 LIDARR_IP="172.20.0.201"
-QBIT_CONF="/srv/torrent-client/qbittorrent/config/qBittorrent/qBittorrent.conf"
-
-if [ -f "$QBIT_CONF" ] && ! grep -q "$LIDARR_IP" "$QBIT_CONF"; then
-  docker stop qbittorrent 2>/dev/null || true
-  sudo sed -i "s|WebUI\\\\AuthSubnetWhitelist=\(.*\)|WebUI\\\\AuthSubnetWhitelist=\1,$LIDARR_IP/32|" "$QBIT_CONF"
-  docker start qbittorrent 2>/dev/null || true
-fi
+RADARR_IP="172.20.0.202"
 
 USER_UID="$(id $SVC_USER -u)" \
   USER_GID="$(id $SVC_USER -g)" \
   LIDARR_IP="$LIDARR_IP" \
+  RADARR_IP="$RADARR_IP" \
   docker compose up -d
